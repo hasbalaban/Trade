90cfd8ec40abae130c40a40b762b87f2c95a0e6f
Hasan Balaban <hasan.balaban@demirorenteknoloji.com>
2024-07-10 16:09:10 +0300

    set scrollBehavior for bottom bar and top app bar

--- app/src/main/java/com/finance/trade_learn/view/home/homePage.kt
+++ app/src/main/java/com/finance/trade_learn/view/home/homePage.kt
@@ -26,9 +26,16 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
@@ -36,6 +43,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
@@ -54,6 +62,9 @@ import com.finance.trade_learn.view.coin.PopularCoinCard
 import kotlinx.coroutines.runBlocking
 
 
+@OptIn(ExperimentalMaterial3Api::class)
+private val LocalHomePageScrollBehavior = compositionLocalOf<TopAppBarScrollBehavior> { error("No TopAppBarScrollBehavior found") }
+
 private fun clickSendEmailButton( context: Context) {
     composeEmail(arrayOf("learntradeapp@gmail.com"),"A intent or Request", context)
 }
@@ -67,64 +78,79 @@ private fun composeEmail(addresses: Array<String>, subject: String, context: Con
     }catch (_:Exception){ }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun MainToolbar(openSearch: () -> Unit) {
-    ConstraintLayout(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(color = MaterialTheme.colors.primary)
-            .padding(horizontal = 8.dp, vertical = 4.dp)
-    ) {
-        val context = LocalContext.current
-        val (composeEmail, appName, search) = createRefs()
+    val scrollBehavior = LocalHomePageScrollBehavior.current
 
-        Icon(
-            imageVector = Icons.Default.Email,
-            contentDescription = "Send Email",
-            modifier = Modifier
-                .constrainAs(composeEmail) {
-                    top.linkTo(parent.top)
-                    start.linkTo(parent.start)
-                }
-                .size(36.dp)
-                .clickable { clickSendEmailButton(context) },
-            tint = MaterialTheme.colors.onPrimary
-        )
+    TopAppBar(
+        modifier = Modifier.padding(top = 6.dp),
+        title = {
+            ConstraintLayout(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = MaterialTheme.colors.primary)
+                    .padding(horizontal = 8.dp, vertical = 4.dp)
+            ) {
+                val context = LocalContext.current
+                val (composeEmail, appName, search) = createRefs()
 
-        Text(
-            text = stringResource(id = R.string.app_name),
-            textAlign = TextAlign.Center,
-            color = MaterialTheme.colors.onPrimary,
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            fontFamily = FontFamily.Default,
-            modifier = Modifier
-                .constrainAs(appName) {
-                    start.linkTo(composeEmail.end)
-                    end.linkTo(search.start)
-                    top.linkTo(parent.top)
-                    bottom.linkTo(parent.bottom)
-                    width = Dimension.fillToConstraints
-                }
-                .padding(horizontal = 8.dp)
-        )
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = "Send Email",
+                    modifier = Modifier
+                        .constrainAs(composeEmail) {
+                            top.linkTo(parent.top)
+                            start.linkTo(parent.start)
+                        }
+                        .size(36.dp)
+                        .clickable { clickSendEmailButton(context) },
+                    tint = MaterialTheme.colors.onPrimary
+                )
 
-        Icon(
-            imageVector = Icons.Default.Search,
-            contentDescription = "Search",
-            modifier = Modifier
-                .constrainAs(search) {
-                    end.linkTo(parent.end)
-                    top.linkTo(parent.top)
-                }
-                .size(36.dp)
-                .clickable { openSearch() },
-            tint = MaterialTheme.colors.onPrimary
-        )
-    }
+                Text(
+                    text = stringResource(id = R.string.app_name),
+                    textAlign = TextAlign.Center,
+                    color = MaterialTheme.colors.onPrimary,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.Default,
+                    modifier = Modifier
+                        .constrainAs(appName) {
+                            start.linkTo(composeEmail.end)
+                            end.linkTo(search.start)
+                            top.linkTo(parent.top)
+                            bottom.linkTo(parent.bottom)
+                            width = Dimension.fillToConstraints
+                        }
+                        .padding(horizontal = 8.dp)
+                )
+
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search",
+                    modifier = Modifier
+                        .constrainAs(search) {
+                            end.linkTo(parent.end)
+                            top.linkTo(parent.top)
+                        }
+                        .size(36.dp)
+                        .clickable { openSearch() },
+                    tint = MaterialTheme.colors.onPrimary
+                )
+            }
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(),
+        scrollBehavior = scrollBehavior
+    )
+
+
+
+
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainView(
     page: Int = 1,
@@ -144,6 +170,10 @@ fun MainView(
         mutableStateOf(60000L)
     }
 
+
+
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
     val popularItems = baseViewModel.listOfCryptoForPopular.observeAsState().value
 
     DisposableEffect(Unit) {
@@ -161,93 +191,107 @@ fun MainView(
         }
     }
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        val isLoading = baseViewModel.isLoading.observeAsState().value ?: false
-        if (isLoading){
-            Row(modifier = Modifier.fillMaxSize()) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    CircularProgressIndicator(
-                        color = colorResource(id = R.color.pozitive),
-                    )
+
+    CompositionLocalProvider(LocalHomePageScrollBehavior provides scrollBehavior) {
+        Scaffold(modifier = Modifier.fillMaxSize().nestedScroll(scrollBehavior.nestedScrollConnection),
+            topBar = {
+                MainToolbar(openSearch)
+            }
+        ) {
+
+            val isLoading = baseViewModel.isLoading.observeAsState().value ?: false
+            if (isLoading){
+                Row(modifier = Modifier.fillMaxSize()) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        CircularProgressIndicator(
+                            color = colorResource(id = R.color.pozitive),
+                        )
+                    }
                 }
             }
-        }
-        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-            val (toolbar, divider1, mainItemsScreen) = createRefs()
-            Column(modifier = Modifier
-                .fillMaxWidth()
-                .constrainAs(toolbar) {
-                    top.linkTo(parent.top)
-                }) {
-                MainToolbar(openSearch)
-
-                Row(modifier = Modifier
+            ConstraintLayout(modifier = Modifier.padding(it).fillMaxSize()) {
+                val (toolbar, divider1, mainItemsScreen) = createRefs()
+                Column(modifier = Modifier
                     .fillMaxWidth()
-                    .height(1.dp)
-                    .background(color = colorResource(id = R.color.light_grey))) {}
+                    .constrainAs(toolbar) {
+                        top.linkTo(parent.top)
+                    }) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(1.dp)
+                            .background(color = colorResource(id = R.color.light_grey))
+                    ) {}
 
-                if (shouldShowPopularCoins){
-                    Column(modifier = Modifier
-                        .verticalScroll(rememberScrollState())
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.popular_coins),
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.Bold,
-                            modifier = Modifier.padding(top = 6.dp, start = 12.dp),
-                            color = MaterialTheme.colors.onPrimary
-                        )
+                    if (shouldShowPopularCoins) {
+                        Column(
+                            modifier = Modifier
+                                .verticalScroll(rememberScrollState())
+                        ) {
+                            Text(
+                                text = stringResource(id = R.string.popular_coins),
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.padding(top = 6.dp, start = 12.dp),
+                                color = MaterialTheme.colors.onPrimary
+                            )
 
-                        if (popularItems != null){
-                            LazyRow(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(vertical = 4.dp)
-                            ) {
-                                items(popularItems) { item ->
-                                    PopularCoinCard(item, Modifier.weight(1f)){selectedItemName ->
-                                        openTradePage.invoke(selectedItemName)
+                            if (popularItems != null) {
+                                LazyRow(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(vertical = 4.dp)
+                                ) {
+                                    items(popularItems) { item ->
+                                        PopularCoinCard(
+                                            item,
+                                            Modifier.weight(1f)
+                                        ) { selectedItemName ->
+                                            openTradePage.invoke(selectedItemName)
+                                        }
                                     }
                                 }
                             }
+
+                        }
+                    }
+
+                }
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = colorResource(id = R.color.light_grey))
+                    .height(1.dp)
+                    .constrainAs(divider1) {
+                        top.linkTo(toolbar.bottom)
+                    }
+                ) {}
+
+                Column(modifier = Modifier
+                    .fillMaxWidth()
+                    .constrainAs(mainItemsScreen) {
+                        top.linkTo(divider1.bottom)
+                        bottom.linkTo(parent.bottom)
+                        height = Dimension.fillToConstraints
+                    }
+
+                ) {
+
+                    val listOfItems = baseViewModel.currentItemsLiveData.observeAsState()
+                    val updateList =
+                        if (shouldShowPopularCoins) listOfItems.value else listOfItems.value?.sortedBy {
+                            it.total_volume
                         }
 
+                    HomePageItems(coinsHome = updateList) { selectedItemName ->
+                        openTradePage.invoke(selectedItemName)
                     }
-                }
-
-            }
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .background(color = colorResource(id = R.color.light_grey))
-                .height(1.dp)
-                .constrainAs(divider1) {
-                    top.linkTo(toolbar.bottom)
-                }
-            ) {}
-
-            Column(modifier = Modifier
-                .fillMaxWidth()
-                .constrainAs(mainItemsScreen) {
-                    top.linkTo(divider1.bottom)
-                    bottom.linkTo(parent.bottom)
-                    height = Dimension.fillToConstraints
-                }
-
-            ) {
-
-                val listOfItems = baseViewModel.currentItemsLiveData.observeAsState()
-                val updateList = if(shouldShowPopularCoins) listOfItems.value else listOfItems.value?.sortedBy {
-                    it.total_volume
-                }
-
-                HomePageItems(coinsHome = updateList){selectedItemName->
-                    openTradePage.invoke(selectedItemName)
                 }
             }
         }
     }
+
 
 }
