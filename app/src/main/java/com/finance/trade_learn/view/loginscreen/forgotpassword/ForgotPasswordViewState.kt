a481881f670bfe2e1c246dd5a9a746f0e772147b
Hasan Balaban <hasan.balaban@demirorenteknoloji.com>
2024-09-12 14:02:44 +0300

    add escape unix code for france language

--- app/src/main/java/com/finance/trade_learn/view/loginscreen/forgotpassword/ForgotPasswordViewState.kt
+++ app/src/main/java/com/finance/trade_learn/view/loginscreen/forgotpassword/ForgotPasswordViewState.kt
@@ -1,6 +1,7 @@
 package com.finance.trade_learn.view.loginscreen.forgotpassword
 
 data class ForgotPasswordViewState(
-    var email : String = "hasan-balaban@hotmail.com",
+    var email : String = "",
+    //var email : String = "hasan-balaban@hotmail.com",
     var isLoading : Boolean = false
 )
