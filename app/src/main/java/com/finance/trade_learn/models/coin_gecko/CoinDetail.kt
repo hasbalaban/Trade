4fb90eaf1e3c72bdb4aa40c3d5a0850be4ed886d
Hasan Balaban <hasan.balaban@demirorenteknoloji.com>
2024-09-21 13:16:58 +0300

    change market api to optimize data size

--- app/src/main/java/com/finance/trade_learn/models/coin_gecko/CoinDetail.kt
+++ app/src/main/java/com/finance/trade_learn/models/coin_gecko/CoinDetail.kt
@@ -1,20 +1,42 @@
 package com.finance.trade_learn.models.coin_gecko
 
+import com.google.gson.annotations.SerializedName
+
 data class CoinDetail(
+    @SerializedName("id")
     val id: String,
+
+    @SerializedName("sm")
     val symbol: String,
+
+    @SerializedName("n")
     val name: String,
+
+    @SerializedName("im")
     val image: String,
+
+    @SerializedName("cp")
     val current_price: Double?,
+
+    @SerializedName("mc")
     val market_cap: String,
-    val market_cap_rank: Double?,
-    val fully_diluted_valuation: String?,
+
+    @SerializedName("tv")
     val total_volume: String,
+
+    @SerializedName("pc")
+    val price_change_24h: Double?,
+
+    @SerializedName("pcp")
+    val price_change_percentage_24h: Double?,
+
+    @SerializedName("mcr")
+    val market_cap_rank: Double?,
+
+
+    val fully_diluted_valuation: String?,
     val high_24h: String?,
     val low_24h: String?,
-    //check here it was string
-    val price_change_24h: Double?,
-    val price_change_percentage_24h: Double?,
     val market_cap_change_24h: String,
     val market_cap_change_percentage_24h: Double?,
     val circulating_supply: String?,
